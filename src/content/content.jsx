f3b546d557939a306f12e7d510de4e08b5829e23
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-07-08 10:41:16 -0300

    fix: Altera nome da advogada

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -53,7 +53,7 @@ import { FileText } from "lucide-react";
 const currentYear = new Date().getFullYear();
 
 export const infos = {
-  name: "Dra. Catarina Mozarkel",
+  name: "Dra. Catarina Mokarzel",
   email: "contato", // Email desejado pelo cliente
   emailSecundario: "A_Definir", // Email desejado pelo cliente
   domain: "catarinamokarzel.com.br", // Sem "www"
@@ -398,7 +398,7 @@ const content = {
         img: aboutImg1,
         alt: "Imagem profissional de escritório/advogado(a)",
       },
-      miniTag: "QUEM É A DRA. CATARINA MOZARKEL",
+      miniTag: "QUEM É A DRA. CATARINA Mokarzel",
       title: "Minha trajetória",
       subtitle: "Quando a vivência vira vocação",
       paragraph: (
@@ -485,7 +485,7 @@ const content = {
           alt: `Foto do Instagram do ${infos.name}`,
         },
         miniTag: "REDES SOCIAIS",
-        title: "Conecte-se conosco",
+        title: "QUEM É A DRA. CATARINA MOKARZEL",
         subtitle:
           "Aproveite nossas redes sociais para mantermos contato e ficar por dentro de atualizações importantes no mundo do direito.",
         paragraph: <p></p>,
